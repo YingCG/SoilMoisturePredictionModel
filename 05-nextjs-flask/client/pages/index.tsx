23fbe6c019015a05929e9c3057cf0302a8b70f61
YingCG <yingchiugoh@gmail.com>
2024-11-11 22:28:26 +1300

    adding frontend

---
+++ 05-nextjs-flask/client/pages/index.tsx
@@ -0,0 +1,43 @@
+import React, {useEffect, useState} from 'react'
+
+interface WeatherData {
+  message: string;
+  rain: number[];
+  airTemp: number[];
+  soilTemp: number[];
+  soilMoisture: number[];
+}
+
+function index() {
+  const [message, setMessage] = useState("Loading")
+  const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/home").then(
+      response => response.json()
+    ).then((data) => {
+      console.log(data)
+      setMessage(data.message)
+      console.log(data.rain)
+      setWeatherData(data)
+    })
+  }, [])
+
+  return (
+    <div>
+      {message}
+      {weatherData && Object.keys(weatherData).map((key) => (
+          key !== "message" && Array.isArray(weatherData[key as keyof WeatherData]) && (
+            <div key={key}>
+              <h2>{key.charAt(0).toUpperCase() + key.slice(1)} Data:</h2>
+              {(weatherData[key as keyof WeatherData] as number[]).map((item, index) => (
+                <p key={index}>{item}</p>
+              ))}
+            </div>
+          )
+        ))}
+    </div>
+  )
+}
+
+export default index;
---
+++ 05-nextjs-flask/server/server.py
@@ -0,0 +1,34 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+import pandas as pd
+
+
+app = Flask(__name__)
+CORS(app)
+
+def load_data_from_csv(file_path):
+    # Read the CSV file
+    data = pd.read_csv(file_path)
+    
+    rain = data['Rain(mm)'].astype(float).tolist()
+    soilTemp = data['SoilTemp(c)'].astype(float).tolist()
+    soilMoisture = data['SoilMoisture(%)'].astype(float).tolist()
+
+    return rain, soilTemp, soilMoisture
+
+rain, soilTemp, soilMoisture = load_data_from_csv('../../Data/soil data/AcrossAucklandRainfallStation/ArarimuCombined_Data_15mins_SoilProfile.csv')
+
+
+@app.route('/api/home', methods=["GET"])
+def return_home():
+    return jsonify({
+        "message": "Lasr 3 days of weather data",
+        "rain": rain[-3: ],
+         "soilTemp": soilTemp[-3: ],
+        'soilMoisture': soilMoisture[-3: ]
+    })
+    
+if __name__ == "__main__":
+    app.run(debug=True, port=8080)
+    
+# https://www.youtube.com/watch?v=OwxxCibSFKk
